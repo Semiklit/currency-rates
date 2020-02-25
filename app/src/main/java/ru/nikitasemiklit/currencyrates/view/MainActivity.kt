c006573236d7e385f6dad064d414bf929ceea82c
Nikita Semiklit <Semiklit@gmail.com>
2020-02-25 23:30:13 +0500

    gradle update + view binding

--- app/src/main/java/ru/nikitasemiklit/currencyrates/view/MainActivity.kt
+++ app/src/main/java/ru/nikitasemiklit/currencyrates/view/MainActivity.kt
@@ -2,76 +2,76 @@ package ru.nikitasemiklit.currencyrates.view
 
 import android.os.Bundle
 import android.view.View
-import android.widget.*
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.observe
-import ru.nikitasemiklit.currencyrates.R
+import ru.nikitasemiklit.currencyrates.databinding.ActivityMainBinding
 import ru.nikitasemiklit.currencyrates.model.RatesViewModel
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val userInputET = findViewById<EditText>(R.id.et_user_input)
-        val resultTV = findViewById<TextView>(R.id.tv_convert_result)
-        val baseSpinner = findViewById<Spinner>(R.id.sp_base_currency)
-        baseSpinner.adapter =
+        val binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root);
+        binding.spBaseCurrency.adapter =
             ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line)
 
-        val targetSpinner = findViewById<Spinner>(R.id.sp_target_currency)
-        targetSpinner.adapter =
+        binding.spTargetCurrency.adapter =
             ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line)
 
-        val viewModel = ViewModelProvider(this).get(RatesViewModel::class.java)
-        userInputET.addTextChangedListener { text ->
+        val viewModel: MainActivityModelInterface =
+            ViewModelProvider(this).get(RatesViewModel::class.java)
+        binding.etUserInput.addTextChangedListener { text ->
             viewModel.userInputValue = text.toString()
         }
         viewModel.baseCurrencies.observe(this) { value ->
-            val adapter = baseSpinner.adapter as ArrayAdapter<String>
+            val adapter = binding.spBaseCurrency.adapter as ArrayAdapter<String>
             adapter.clear()
             adapter.addAll(value)
         }
         viewModel.targetCurrencies.observe(this) { value ->
-            val adapter = targetSpinner.adapter as ArrayAdapter<String>
+            val adapter = binding.spTargetCurrency.adapter as ArrayAdapter<String>
             adapter.clear()
             adapter.addAll(value)
         }
         viewModel.targetValue.observe(this) { value ->
-            resultTV.text = value
+            binding.tvConvertResult.text = value
         }
 
-        baseSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        binding.spBaseCurrency.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    viewModel.baseCurrency = parent?.getItemAtPosition(position) as String
+                }
             }
 
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                viewModel.baseCurrency = parent?.getItemAtPosition(position) as String
-            }
-        }
+        binding.spTargetCurrency.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
 
-        targetSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    viewModel.targetCurrency = parent?.getItemAtPosition(position) as String
+                }
             }
-
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                viewModel.targetCurrency = parent?.getItemAtPosition(position) as String
-            }
-        }
     }
 }
